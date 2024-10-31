70680341424b0b1f2dca8f42309e31eadfc2bade
QobilovSardor <note47297@gmail.com>
2024-11-01 01:40:40 +0500

    S

--- js/main.js
+++ js/main.js
@@ -59,3 +59,28 @@ let aboutUsCardsSwiper = new Swiper('.about-us__cards-swiper', {
     }
   }
 });
+
+
+window.addEventListener("scroll", function () {
+  const menu = document.querySelector(".fixed-menu");
+  if (window.scrollY === 0) {
+    menu.classList.add("top");
+    menu.classList.remove("scrolled");
+  } else {
+    menu.classList.add("scrolled");
+    menu.classList.remove("top");
+  }
+});
+
+const menuBtn = document.querySelector('.menu-img');
+const fixedMenu = document.querySelector('.fixed-menu');
+
+menuBtn.addEventListener('click', () => {
+  fixedMenu.classList.toggle('show');
+})
+
+window.addEventListener('click', (event) => {
+  if (!menuBtn.contains(event.target) && !fixedMenu.contains(event.target)) {
+    fixedMenu.classList.remove('show');
+  }
+});
